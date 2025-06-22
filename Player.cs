6fd8d659006db973004589bd4a716263952cd7f1
csesznakpatrik1 <tkueug@hallgato.nje.hu>
2025-06-22 15:04:23 +0200

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FighterGame
+{
+   public enum Attacks
+    {
+        Basic,
+        Crit,
+        Heal,
+        Double
+
+    }
+
+    abstract class  Player
+    {
+        
+        private int hp;
+        private int damage;
+        private int stamina;
+        private readonly int maxStamina;
+        private readonly int maxHp;
+        public abstract List<Attacks> GetAvailableMoves();
+
+        //get set
+        public int Stamina
+        {
+            get { return stamina; }
+            protected set { stamina = Mathf.Clamp(value, 0, MaxStamina); }
+        }
+        public int Damage
+        {
+            get { return damage; }
+            protected set { damage = value; }
+        }
+
+
+
+        public int HP
+        {
+            get { return hp; }
+            protected set
+            {
+                if (value < 0)
+                {
+                    hp = 0;
+                }
+                else
+                {
+                    hp = Mathf.Clamp(value, 0, MaxHP);
+                }
+            }
+        }
+
+        public int MaxHP
+        {
+            get { return maxHp; }
+        }
+        public int MaxStamina
+        {
+            get { return maxStamina; }
+        }
+
+
+        public Player(int maxHp, int damage, int maxStamina)
+        {
+            this.maxHp = maxHp;
+            this.hp = maxHp;
+
+            this.damage = damage;
+
+            this.maxStamina = maxStamina;
+            this.stamina = maxStamina;
+        }
+       
+
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+        }
+        public void DoDamage(Player player)
+        {
+            if (Stamina > maxStamina * 0.1)
+            {
+            player.TakeDamage(Damage);
+            Stamina -= (int)(Stamina * 0.1);
+
+            }
+        }
+
+
+    }
+
+    class Tank : Player
+    {
+        private readonly double healPercent = 0.1;
+        public Tank() : base(maxHp: 300,damage: 20,maxStamina: 120) { }
+        public void Heal()
+        {
+            Random rand = new Random();
+            int randomHeal = rand.Next(0, 2);
+
+            if (randomHeal == 0)
+            {
+                Console.WriteLine("Nem sikerült a heal");
+            }
+            else if (randomHeal == 1)
+            {
+                Console.WriteLine("Sikerült a heal");
+
+                HP += (int)(MaxHP * healPercent);
+                Stamina -= (int)(MaxStamina * 0.15);
+            }
+
+        }
+
+        public override List<Attacks> GetAvailableMoves()
+        {
+            return new List<Attacks> {Attacks.Basic, Attacks.Heal };
+        }
+
+
+    }
+
+    class GlassCannon : Player
+    {
+        private readonly int critMultiplier = 2;
+        public override List<Attacks> GetAvailableMoves()
+        {
+            return new List<Attacks> { Attacks.Basic, Attacks.Crit };
+        }
+        public void CritAttack(Player player)
+        {
+            Random random = new Random();
+            int critChance = random.Next(0, 2);
+
+            if (critChance == 0)
+            {
+                Console.WriteLine("Nem sikerült a támadás!");
+            }
+            else if (critChance == 1)
+            {
+                Console.WriteLine("Sikerült a támadás");
+
+                player.TakeDamage(Damage * critMultiplier);
+                Stamina -= (int)(MaxStamina * 0.15);
+            }
+
+        }
+
+ 
+        public GlassCannon() :base (maxHp: 50,damage: 120,maxStamina: 60) { }
+
+
+    }
+
+
+    class Scout : Player
+    {
+        private readonly int multipleAttack = 2;
+        public override List<Attacks> GetAvailableMoves()
+        {
+            return new List<Attacks> { Attacks.Basic, Attacks.Double };
+        }
+        public void DoubleAttack(Player player)
+        {
+
+
+            Random rand = new Random();
+            int doubleAttackChance = rand.Next(0, 2);
+
+            if (doubleAttackChance == 0)
+            {
+                Console.WriteLine("Nem sikerült a dupla támadás");
+            }
+            else if (doubleAttackChance == 1)
+            {
+                Console.WriteLine("Sikerült a dupla támadás");
+
+                for (int i = 0; i < multipleAttack; i++)
+                {
+                    DoDamage(player);
+                }
+            }
+
+            
+        }
+
+
+
+        public Scout()  :base(maxHp: 70,damage: 30,maxStamina: 200) { }
+ 
+
+    }
+
+    class Mathf
+    {
+        public static int Clamp(int value, int min, int max)
+        {
+            if (value < min) { return min; } else if (value > max) { return max; }
+            return value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FighterGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // players setup
+              Console.WriteLine("Player 1");
+              Player player1 = SelectPlayer();
+              Console.WriteLine("Player 2");
+              Player player2 = SelectPlayer();
+
+              Console.Clear();
+            /*
+              // game
+              while (player1.HP > 0 && player2.HP > 0)
+              {
+
+                  //shows the stats at the beginning of every round
+                  ShowStats(player1, player2);
+                  // gets the round winner in the guessing game, the round winner can attack
+                  int winner = NextPlayer();
+
+
+                  //attack mechanism
+                  if (winner == 0) 
+                  {
+                      HandlePlayerMove(player1, player2);
+
+                  }
+                  else if (winner == 1)
+                  {
+                      HandlePlayerMove(player2, player1);
+                  }
+
+                  //wait for a bit after the round
+                  System.Threading.Thread.Sleep(3000);
+                  Console.Clear();
+
+              } */
+
+            HandlePlayerMove(player1,player2);
+        }
+
+
+        static void HandlePlayerMove(Player current, Player target)
+        {
+            Console.WriteLine("Válaszd ki mit szeretnél csinálni: ");
+
+            //getavailablemoves call, majd ezzel torteno move vegrehajtasa!!!!
+            List<Attacks> currentMoves = current.GetAvailableMoves();
+            int input;
+            do
+            {
+                for (int i = 0; i < currentMoves.Count; i++)
+                {
+                    Console.WriteLine($"({i + 1}): {currentMoves[i]}");
+
+                }
+                int.TryParse(Console.ReadLine(), out input);
+
+            } while (input < 1 || input > 3);
+
+
+        }
+
+
+        static void ShowStats(Player player1, Player player2)
+        {
+            Console.WriteLine($"1-es Játékos statisztikái: \n \n Neve:{player1.GetType().Name} \n HP:{player1.HP} \n Damage:{player1.Damage} \n Stamina:{player1.Stamina} \n");
+            Console.WriteLine($"2-es Játékos statisztikái: \n \n Neve:{player2.GetType().Name}\n HP:{player2.HP} \n Damage:{player2.Damage} \n Stamina:{player2.Stamina} \n");
+        }
+        
+        static int NextPlayer()
+        {
+            Random random = new Random();
+            int GuessNumber = random.Next(0, 10);
+            Console.WriteLine("1. Játékos tippeljen 1-10 között!");
+            int.TryParse(Console.ReadLine(), out int player1GuessNumber);
+
+            Console.WriteLine("2. Játékos tippeljen 1-10 között!");
+            int.TryParse(Console.ReadLine(), out int player2GuessNumber);
+
+            player1GuessNumber -= GuessNumber;
+            player1GuessNumber = Math.Abs(player1GuessNumber);
+
+            player2GuessNumber -= GuessNumber;
+            player2GuessNumber = Math.Abs(player2GuessNumber);
+
+            if (player1GuessNumber < player2GuessNumber)
+            {
+                Console.WriteLine("Az első játékos nyert!");
+                return 0;
+            }
+            else
+            {
+                Console.WriteLine("A második játékos nyert!");
+                return 1;
+            }
+
+        }
+        static Player SelectPlayer()
+        {
+            int selectedPlayerNum;
+            do
+            {
+
+                Console.WriteLine("Kérem válaszd ki a karaktered: \n (1) Tank \n (2) GlassCannon \n (3) Scout");
+                int.TryParse(Console.ReadLine(), out selectedPlayerNum);
+            } while (selectedPlayerNum > 3 || selectedPlayerNum < 1);
+
+
+            switch (selectedPlayerNum)
+            {
+                case 1: return new Tank();
+                case 2: return new GlassCannon();
+                case 3: return new Scout();
+                    default: throw new Exception("nem megfelelo a mukodes");
+            }
+            
+            
+        }
+    }
+}
